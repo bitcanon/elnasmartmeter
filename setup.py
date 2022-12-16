ede852a543892a67c611ef33e73a6e8e27450884
Mikael Schultz <bitcanon@pm.me>
2022-12-16 19:02:05 +0100

    Update to version 1.1.1.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='elnasmartmeter',
-    version='1.1.0',
+    version='1.1.1',
     author='Mikael Schultz',
     author_email='bitcanon@pm.me',
     description='A simple library for the E.ON Elna API written in Python 3.',
